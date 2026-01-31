40635777f03b4884c44fac75dabaa410c03a8473
Dhvan <dhvanshah15@gmail.com>
2026-01-30 21:16:58 -0500

    eod 1

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import time
 from servo_array import Servo_Array
+from servo import Servo
+
 # 1. Pick your pin
-PINS = ["D2", "D4"]
+PINS = ["D2", "D6"]
+
+# servo = Servo("D2", min_us=500, max_us=2600)
+# servo.set_angle(0)
+# time.sleep(2.5)
+# servo.set_angle(180)
+# time.sleep(2.5)
 
 servo_array = Servo_Array(PINS, [0, 0], 2)
-
-servo_array.move_servos([1.0, 1.0])
-time.sleep(1)
 servo_array.move_servos([0.0, 0.0])
+time.sleep(1.0)
+servo_array.move_servos([0.0, 1.0])
+time.sleep(1.0)
+servo_array.move_servos([0.0, 0.0])
+
+print("Servo DONE!")
--- stepper.py
+++ stepper.py
@@ -1,3 +1,46 @@
-class Stepper:
-    def __init__(self):
-        pass
+import pyb
+import time
+
+class DoubleStepper:
+    def __init__(self, s1_pins, s2_pins):
+        self.s1_pins = (pyb.Pin(s1_pins[0], pyb.Pin.OUT_PP), pyb.Pin(s1_pins[1], pyb.Pin.OUT_PP))
+        self.s2_pins = (pyb.Pin(s2_pins[0], pyb.Pin.OUT_PP), pyb.Pin(s2_pins[1], pyb.Pin.OUT_PP))
+        
+        self.s1_position = 0
+        self.s2_position = 0
+        
+    def set_step_high(self):
+        self.s1_pins[0].high()
+        self.s2_pins[0].high()
+        
+    def set_step_low(self):
+        self.s1_pins[0].low()
+        self.s2_pins[0].low()
+        
+    def set_dir_high(self):
+        self.s1_pins[1].high()
+        self.s2_pins[1].high()
+        
+    def set_dir_low(self):
+        self.s1_pins[1].low()
+        self.s2_pins[1].low()
+        
+    def move(self, steps, speed_delay=0.0001):
+        if steps > 0:
+            self.set_dir_high()
+            direction = 1
+        else:
+            self.set_dir_low()
+            direction = -1
+            
+        count = abs(steps)
+        for _ in range(count):
+            self.set_step_high()
+            time.sleep_us(2) # type: ignore
+            self.set_step_low()
+            
+            self.position += direction
+            
+            time.sleep(speed_delay)
+            
+        
